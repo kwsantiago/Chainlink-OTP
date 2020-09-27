9ebc09cddec79ac6511cbd41fdc57cb81607d1e5
kwsantiago <kwsantiago@usf.edu>
2020-09-27 15:58:23 -0400

    Add VRF to Title

--- OTP-frontend/src/shared/UI/Card.js
+++ OTP-frontend/src/shared/UI/Card.js
@@ -3,7 +3,7 @@ import { Card, Heading } from "rimble-ui";
 
 const CardComponent = ({ children }) => (
   <Card width={"auto"} maxWidth={"600px"} mx={"auto"} px={[3, 3, 4]}>
-    <Heading>Chainlink One-time Pad (OTP)</Heading>
+    <Heading>Chainlink VRF One-time Pad (OTP)</Heading>
     {children}
   </Card>
 );
