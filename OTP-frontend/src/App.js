3e66731b0615946f994a38ef0ed5915c504e4e58
kwsantiago <kwsantiago@usf.edu>
2020-10-03 10:10:16 -0400

    Import VRF function

--- OTP-frontend/src/App.js
+++ OTP-frontend/src/App.js
@@ -5,6 +5,7 @@ import withTheme from "./shared/HOCs/withTheme";
 import useWeb3 from "./shared/hooks/withWeb3";
 import { EthAddress } from "rimble-ui";
 import Card from "./shared/UI/Card";
+import getVRF from "./shared/smartContract/index"
 const DisplayAddress = ({ account = "" }) => <EthAddress address={account} />;
 const App = (props) => {
   const { account, web3 } = useWeb3();
@@ -12,6 +13,7 @@ const App = (props) => {
     <Card>
       {account.length > 1 && <DisplayAddress account={account} />}
       <OTPGenerator />
+      <getVRF />
     </Card>
   );
 };
