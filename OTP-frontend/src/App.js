84790337a588a817e6d987340caf880f41465ee5
kwsantiago <kwsantiago@usf.edu>
2020-10-07 21:32:06 -0400

    Remove import

--- OTP-frontend/src/App.js
+++ OTP-frontend/src/App.js
@@ -5,7 +5,7 @@ import withTheme from "./shared/HOCs/withTheme";
 import useWeb3 from "./shared/hooks/withWeb3";
 import { EthAddress } from "rimble-ui";
 import Card from "./shared/UI/Card";
-import getVRF from "./shared/smartContract/index"
+
 const DisplayAddress = ({ account = "" }) => <EthAddress address={account} />;
 const App = (props) => {
   const { account, web3 } = useWeb3();
@@ -13,7 +13,6 @@ const App = (props) => {
     <Card>
       {account.length > 1 && <DisplayAddress account={account} />}
       <OTPGenerator />
-      <getVRF />
     </Card>
   );
 };
