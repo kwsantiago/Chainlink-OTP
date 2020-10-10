1079af5f660a3b3b9117a3e1a0e48bf2f8ff0c3a
kwsantiago <kwsantiago@usf.edu>
2020-10-09 22:49:55 -0400

    Remove Custom Pad Length

--- OTP-frontend/src/assets/utils/otpGenerator.js
+++ OTP-frontend/src/assets/utils/otpGenerator.js
@@ -27,12 +27,10 @@ const Results = ({ pads }) => (
 );
 
 const OTPGenerator = () => {
-  const [padLength, setPadLength] = useState(6);
   const [result, setResult] = useState([]);
 
-  const onGeneratePads = async function otpGen() {
-    console.log(padLength);
-    const result = await genPads(6);
+  const generatePads = async function otpGen() {
+    const result = await genPads(7);
     console.log({ result });
     setResult(result);
   };
@@ -44,20 +42,15 @@ const OTPGenerator = () => {
   };
   return (
     <Box mt={3}>
-      <div>Pad Length</div>
-      <Input
-        name="padLength"
-        id="padLength"
-        type="number"
-        value={padLength}
-        placeholder="Set Pad Length"
-        onChange={setter(setPadLength)}
-      />
-      <div><Button
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            }}><Button
         width={[1, "auto", "auto"]}
         mt={2}
         mb={2}
-        onClick={onGeneratePads}
+        onClick={generatePads}
       >
         Generate Pads
       </Button></div>
