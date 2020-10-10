9d14e197381ce7ad3b09973f3fac8e22c250df8b
kwsantiago <kwsantiago@usf.edu>
2020-10-09 22:50:06 -0400

    Fix box size

--- OTP-frontend/src/assets/shared/UI/Card.js
+++ OTP-frontend/src/assets/shared/UI/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Card, Heading } from "rimble-ui";
 
 const CardComponent = ({ children }) => (
-  <Card width={"auto"} maxWidth={"600px"} mx={"auto"} px={[3, 3, 4]}>
-    <Heading>Chainlink VRF One-time Pad (OTP)</Heading>
+  <Card width={"auto"} maxWidth={"675px"} mx={"auto"} px={[3, 3, 4]}>
+    <Heading>Chainlink VRF One-Time-Pad Generator</Heading>
     {children}
   </Card>
 );
