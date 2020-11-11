4fb0144162581e926a1a2063ce61814d48f66851
kwsantiago <kwsantiago@usf.edu>
2020-11-10 21:47:21 -0500

    Simplify ABI

--- OTP-frontend/src/assets/utils/smartContract.js
+++ OTP-frontend/src/assets/utils/smartContract.js
@@ -3,11 +3,7 @@ const web3 = new Web3(Web3.givenProvider);
 const { eth } = web3;
 const id = x => x;
 // Set up contract instance
-const abi = [{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
+const abi = [
 	{
 		"inputs": [
 			{
@@ -28,25 +24,6 @@ const abi = [{
 		"type": "function"
 	},
 	{
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "",
-				"type": "bytes32"
-			}
-		],
-		"name": "nonces",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
 		"inputs": [],
 		"name": "randomResult",
 		"outputs": [
@@ -57,60 +34,6 @@ const abi = [{
 			}
 		],
 		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "requestId",
-				"type": "bytes32"
-			},
-			{
-				"internalType": "uint256",
-				"name": "randomness",
-				"type": "uint256"
-			}
-		],
-		"name": "rawFulfillRandomness",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "_keyHash",
-				"type": "bytes32"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_fee",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_seed",
-				"type": "uint256"
-			}
-		],
-		"name": "requestRandomness",
-		"outputs": [
-			{
-				"internalType": "bytes32",
-				"name": "requestId",
-				"type": "bytes32"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "withdrawLink",
-		"outputs": [],
-		"stateMutability": "nonpayable",
 		"type": "function"
 	}
 ]
